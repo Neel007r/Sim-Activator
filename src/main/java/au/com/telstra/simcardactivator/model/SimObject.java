7f81581317c49999166892f7098d335f2b62e830
Neel <neelrpatel007@gmail.com>
2025-11-13 21:41:01 -0500

    enhanced code logic

--- src/main/java/au/com/telstra/simcardactivator/model/SimObject.java
+++ src/main/java/au/com/telstra/simcardactivator/model/SimObject.java
@@ -1,9 +1,13 @@
 package au.com.telstra.simcardactivator.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SimActivationRequest {
 
     private String iccid;
     private String customerEmail;
+    private boolean active;
 
     public String getIccid(){
         return iccid;
